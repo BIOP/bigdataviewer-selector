2d369fe93e1620795349469a0229ac49dd2fdf15
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-12-28 16:35:59 +0100

    attempt to fix deadlock

--- src/main/java/ch/epfl/biop/bdv/select/SourceSelectorOverlay.java
+++ src/main/java/ch/epfl/biop/bdv/select/SourceSelectorOverlay.java
@@ -191,11 +191,13 @@ public class SourceSelectorOverlay extends BdvOverlay {
         this.canvasHeight = height;
     }
 
-    public synchronized void updateBoxes() {
-        sourcesBoxOverlay.clear();
-        for (SourceAndConverter sac : viewer.state().getVisibleSources()) {
-            if (sac.getSpimSource().getSource(viewer.state().getCurrentTimepoint(),0)!=null) { // TODO : fix hack to avoid dirty overlay filter
-                sourcesBoxOverlay.add(new SourceSelectorOverlay.SourceBoxOverlay(sac));
+    public void updateBoxes() {
+        synchronized (sourcesBoxOverlay) {
+            sourcesBoxOverlay.clear();
+            for (SourceAndConverter sac : viewer.state().getVisibleSources()) {
+                if (sac.getSpimSource().getSource(viewer.state().getCurrentTimepoint(), 0) != null) { // TODO : fix hack to avoid dirty overlay filter
+                    sourcesBoxOverlay.add(new SourceSelectorOverlay.SourceBoxOverlay(sac));
+                }
             }
         }
     }
